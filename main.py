be588ec54d9e44b0175ab2b467c806c754448d6e
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-10 01:02:18 +0900

    定数化

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mido.messages.specs import CHANNEL_MESSAGES
 import pretty_midi
 import subprocess
 import time
@@ -6,62 +7,74 @@ import os
 import sounddevice as sd
 import wave
 import numpy as np
+import sys
 
 PLAYER_PATH = "C:\\Program Files (x86)\\TMIDI Player\\TMIDI.EXE"
+REC_AUDIO_DEVICE_IDX = 16
+PLAYBACK_AUDIO_DEVICE_IDX = 16
+MIDI_DEVICE_IDX = 2
+REC_SAMPLE_RATE = 44100
+REC_CHANNELS = 2
+INPUT_CHANNEL_LEFT = 2
+INPUT_CHANNEL_RIGHT = 3
+OUTPUT_CHANNEL_LEFT = 0
+OUTPUT_CHANNEL_RIGHT = 1
+
+MIDI_FILENAME = 'MIRROR.mid'
+
+PADDING_REC_TIME = 2
 
 ports = mido.get_output_names()
-print(str(ports))
-midi_device_name = 'MOTU M Series MIDI Out'
-midi_device_index = ports.index(list(filter(lambda v: midi_device_name in v, ports))[0])
-print('index: '  + str(midi_device_index))
-
 sound_devices = sd.query_devices()
-print(sound_devices)
-sd.default.samplerate = 44100
-sd.default.channels = 2
-sd.default.device = [16,16]#input:output
-print(sound_devices[16])
-# チャンネルマッピング
-# in 3・4を指定
-input_selector = [2, 3]
-# out 1・2を指定(聴かないので指定する意味はない)
-output_selector = [0, 1]
 
+
+if sys.argv[1] == 'get' and sys.argv[2] == 'devices':
+    print(str(ports))
+    print(sound_devices)
+    sys.exit()
+
+sd.default.samplerate = REC_SAMPLE_RATE
+sd.default.channels = REC_CHANNELS
+sd.default.device = [REC_AUDIO_DEVICE_IDX,PLAYBACK_AUDIO_DEVICE_IDX] #input:output
+print(sound_devices[REC_AUDIO_DEVICE_IDX])
+# チャンネルマッピング
+input_selector = [INPUT_CHANNEL_LEFT, INPUT_CHANNEL_RIGHT]
+# out 1・2を指定(聴かないので指定する意味はない)
+output_selector = [OUTPUT_CHANNEL_LEFT, OUTPUT_CHANNEL_RIGHT]
 asio_in = sd.AsioSettings(channel_selectors = input_selector)
 asio_out = sd.AsioSettings(channel_selectors = output_selector)
 sd.default.extra_settings = (asio_in,asio_out)
 
-midi_data = pretty_midi.PrettyMIDI('MIRROR.mid')
+midi_data = pretty_midi.PrettyMIDI(MIDI_FILENAME)
 
 duration_seconds = midi_data.get_end_time()
-# duration_seconds = 10
-additional_seconds = 1
-duration = duration_seconds + additional_seconds
-process = subprocess.Popen(PLAYER_PATH + ' ' + os.path.abspath("MIRROR.MID"))
-recdata = sd.rec(int(duration * sd.default.samplerate))
+
+rec_time_seconds = duration_seconds + PADDING_REC_TIME
+process = subprocess.Popen(PLAYER_PATH + ' ' + os.path.abspath(MIDI_FILENAME))
+recdata = sd.rec(int(rec_time_seconds * REC_SAMPLE_RATE))
 sd.wait()
 process.kill()
 
-# ノートオフしないで終わってもノートオフする
-with mido.open_output(ports[midi_device_index]) as outport:
-    # PANIC
+# 鳴りっぱなしの音を止める
+with mido.open_output(ports[MIDI_DEVICE_IDX]) as outport:
+    # 念のため全チャンネルノートオフ
     for ch in range(16):
         outport.send(mido.Message('note_off', channel=ch))
-    # GS Reset
+    # GS Reset（リバーブ対策）
     sysex_gs_reset = [0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41]
     outport.send(mido.Message('sysex', data=sysex_gs_reset))    
 
-
-wave_filename = "MIRROR.mid" + ".wav"
+wave_filename = MIDI_FILENAME + ".wav"
 # 正規化
 data = recdata / recdata.max() * np.iinfo(np.int16).max
 # float -> int
 data = data.astype(np.int16)
 # ファイル保存
 with wave.open(wave_filename, mode='wb') as wb:
-    wb.setnchannels(2)  # ステレオ
-    wb.setsampwidth(2)  # 2byte(16bit)
-    wb.setframerate(44100)
+    wb.setnchannels(REC_CHANNELS)  # ステレオ
+    quantifying_byte_number = 2 # 16bit
+    wb.setsampwidth(quantifying_byte_number)  # 2byte(16bit)
+    wb.setframerate(REC_SAMPLE_RATE)
     wb.writeframes(data.tobytes())  # バイト配列に変換
 
 
